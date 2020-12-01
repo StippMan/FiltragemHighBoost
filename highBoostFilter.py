64f89271d5731b99fdd3eddb854532f17069c502
StippMan <ra102919@uem.br>
2020-12-01 13:12:41 -0300

    show input image

--- highBoostFilter.py
+++ highBoostFilter.py
@@ -4,13 +4,21 @@ import skimage as ski
 import sys
 import argparse
 
+img_in_filename = 'test_img/blurry_moon.tif'
+
 parser = argparse.ArgumentParser(description='Applies the High-Boost Filter to the input image and returns an output image')
-parser.add_argument('img_file_name', help='input image file (ex. \'folder/img.png\')')
+parser.add_argument('-i','--img_file_name', type=str,
+					help='input image file (ex. \'folder/img.png\')')
+if len(sys.argv) > 1:
+	img_in_filename = parser.parse_args().img_file_name
 
-img_in_filename = parser.parse_args().img_file_name
+img_in = cv2.imread(img_in_filename,1)
+imgfile_folder,imgfile_name = img_in_filename.split('/')
+imgfile_name,imgfile_extension = imgfile_name.split('.')
 
-img_in = cv2.imread("input_images/"+img_in_filename,1)
-imgfile_name,imgfile_extension = img_in_filename.split('.')
+print(imgfile_folder)
+print(imgfile_name)
+print(imgfile_extension)
 cv2.namedWindow('input_imgfile', cv2.WINDOW_NORMAL)
 cv2.imshow("input_imgfile", img_in)
 
