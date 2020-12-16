26ceebea315dd74f1434fd61c3a3179a74d5aa00
StippMan <ra102919@uem.br>
2020-12-16 18:59:57 -0300

    nova tentativa, salvando antes

--- highBoostFilter.py
+++ highBoostFilter.py
@@ -4,7 +4,8 @@ import skimage as ski
 import sys
 import argparse
 
-img_in_filename = "test_img/blurry_moon.tif"
+img_in_filename = "img_in/blurry_moon.tif"
+
 
 parser = argparse.ArgumentParser(
     description="Applies the High-Boost Filter to the input image and returns an output image"
@@ -17,11 +18,21 @@ img_in = cv2.imread(img_in_filename, 1)
 imgfile_folder, imgfile_name = img_in_filename.split("/")
 imgfile_name, imgfile_extension = imgfile_name.split(".")
 
-print(imgfile_folder)
-print(imgfile_name)
-print(imgfile_extension)
+outfile_name = imgfile_name + "_cv"
+img_out_filename = "img_out/" + outfile_name + "." + imgfile_extension
+
+
+img_out = img_in
+
+print(img_in.shape)
+print(img_out.shape)
 cv2.namedWindow("input_imgfile", cv2.WINDOW_NORMAL)
 cv2.imshow("input_imgfile", img_in)
+
+cv2.namedWindow("output_imgfile", cv2.WINDOW_NORMAL)
+cv2.imshow("output_imgfile", img_out)
+
+cv2.imwrite(img_out_filename, img_out)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
